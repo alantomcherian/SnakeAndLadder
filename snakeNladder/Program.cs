34d6120834d87ccec262cec8c5ecc5edf0e91a7d
alantomcherian <atcherian101@gmail.com>
2021-08-05 18:37:52 +0530

    feat: Fininshing Point

--- snakeNladder/Program.cs
+++ snakeNladder/Program.cs
@@ -11,32 +11,39 @@ namespace snakeNladder
         {
             int pos1 = 0;
             int Start = 0;
+            int dice ;
+            int c;
 
             Random r = new Random();
-            int dice = r.Next(1, 7);
-            int c = r.Next(1, 4); 
 
-            switch (c)
+            while (pos1 <= 100)
             {
-                case 1 :
-                    Console.WriteLine("No position change");
-                    break;
-                case 2 :
-                    pos1 += dice;
-                    Console.WriteLine("player has moved to "+ pos1 +" block by "+ dice +" steps.");
-                    break;
-                case 3 :
-                    pos1 -= dice;
-                    if (pos1 <= Start)
-                    {
-                        pos1 = 0;
-                    }
-                    Console.WriteLine("player has moved back to " + pos1 + " block by " + dice + " steps.");
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice");
-                    break;
+                dice = r.Next(1, 7);
+                c = r.Next(1, 4);
+
+                switch (c)
+                {
+                    case 1:
+                        Console.WriteLine("No position change");
+                        break;
+                    case 2:
+                        pos1 += dice;
+                        Console.WriteLine("player has moved to " + pos1 + " block by " + dice + " steps.");
+                        break;
+                    case 3:
+                        pos1 -= dice;
+                        if (pos1 <= Start)
+                        {
+                            pos1 = 0;
+                        }
+                        Console.WriteLine("player has moved back to " + pos1 + " block by " + dice + " steps.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
             }
+            
             
         
         }
