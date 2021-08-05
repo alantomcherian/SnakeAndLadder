ffd18737d3368ee00b67c515f3d64e841ae5aeb5
alantomcherian <atcherian101@gmail.com>
2021-08-05 18:28:21 +0530

    feat: play options

--- snakeNladder/Program.cs
+++ snakeNladder/Program.cs
@@ -10,8 +10,34 @@ namespace snakeNladder
         static void Main(string[] args)
         {
             int pos1 = 0;
-            int StartPos = pos1;
-            Console.WriteLine("player 1 is position" + pos1);
+            int Start = 0;
+
+            Random r = new Random();
+            int dice = r.Next(1, 7);
+            int c = r.Next(1, 4); 
+
+            switch (c)
+            {
+                case 1 :
+                    Console.WriteLine("No position change");
+                    break;
+                case 2 :
+                    pos1 += dice;
+                    Console.WriteLine("player has moved to "+ pos1 +" block by "+ dice +" steps.");
+                    break;
+                case 3 :
+                    pos1 -= dice;
+                    if (pos1 <= Start)
+                    {
+                        pos1 = 0;
+                    }
+                    Console.WriteLine("player has moved back to " + pos1 + " block by " + dice + " steps.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+            
         
         }
     }
